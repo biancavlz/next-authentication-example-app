48cf2f1841ed8fc521cc3fe611996162b2a3d4ff
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-06-02 11:08:28 +0200

    add sigup server action with user input validation

---
+++ actions/auth-actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+type FormState = {
+  errors?: Record<string, string>;
+};
+
+export async function signup(
+  prevState: FormState,
+  formData: FormData,
+): Promise<FormState> {
+  const email = String(formData.get("email") ?? "");
+  const password = String(formData.get("password") ?? "");
+
+  // User input validation
+  const errors: Record<string, string> = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  return {};
+}
--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -1,8 +1,22 @@
-import Link from 'next/link';
+"use client";
+
+import { signup } from "@/actions/auth-actions";
+import Link from "next/link";
+import { useActionState } from "react";
+
+type FormState = {
+  errors?: Record<string, string>;
+};
+
+type PostFormProps = {
+  action: (state: FormState, formData: FormData) => Promise<FormState>;
+};
 
 export default function AuthForm() {
+  const [state, formAction] = useActionState<FormState, FormData>(signup, {});
+
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -14,10 +28,16 @@ export default function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {state.errors && Object.keys(state.errors).length > 0 && (
+        <ul id="form-errors">
+          {Object.keys(state.errors).map((key) => (
+            <li key={key}>{state.errors![key]}</li>
+          ))}
+        </ul>
+      )}
+
       <p>
-        <button type="submit">
-          Create Account
-        </button>
+        <button type="submit">Create Account</button>
       </p>
       <p>
         <Link href="/">Login with existing account.</Link>
